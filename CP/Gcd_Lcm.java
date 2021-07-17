9d0d94884c4ee7420f161c324e84e6789ac16a29
Rupam Hari <hrupam55@gmail.com>
2021-07-17 15:35:16 +0530

    lcm added

--- CP/Gcd_Lcm.java
+++ CP/Gcd_Lcm.java
@@ -8,20 +8,24 @@ package CP;
 import java.util.Scanner;
 
 /**
- *
  * @author rupam
  */
-public class Gcd {
+public class Gcd_Lcm {
 
     public static int gcd(int a, int b) {
         return a % b == 0 ? b : gcd(b, a % b);
+    }
+
+    public static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
     }
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int result = gcd(a, b);
-        System.out.println("GCD = " + result);
+
+        System.out.println("GCD = " + gcd(a, b));
+        System.out.println("LCM = " + lcm(a, b));
     }
 }
