b17b3024fd80650328b66844a2200047e0e1a530
Rupam Hari <hrupam55@gmail.com>
2021-08-04 13:10:31 +0530

    MaximumSumSubarray with index

--- ArrayOps/MaximumSumSubarray.java
+++ ArrayOps/MaximumSumSubarray.java
@@ -16,23 +16,28 @@ public class MaximumSumSubarray {
 
     public static int bruteForce(int[] a) {
         int maxSum = Integer.MIN_VALUE;
+        int start = 0;
+        int end = a.length;
         for (int i = 0; i < a.length; i++) {
             for (int j = i + 1; j <= a.length; j++) {
                 int sum = 0;
                 for (int k = i; k < j; k++) {
                     sum += a[k];
                 }
-                if (maxSum < sum)
+                if (maxSum < sum) {
                     maxSum = sum;
-
+                    start = i;
+                    end = j;
+                }
             }
         }
+        System.out.println("start = " + start + ", end = " + (end - 1));
         return maxSum;
     }
 
     public static void main(String[] args) {
-        int a[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4, 1, 6, 9, 5, 4, -9, 4, -9, 8, 4, 45, 8, -2, 8, 9, -9, -10, -78};
-
+//        int a[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4, 1, 6, 9, 5, 4, -9, 4, -9, 8, 4, 45, 8, -2, 8, 9, -9, -10, -78};
+        int a[] = {-2, 1, 3, -5, 1};
         long bruteForceStartTime = System.nanoTime();
         int maxSumBrute = bruteForce(a);
         long bruteForceEndTime = System.nanoTime();
