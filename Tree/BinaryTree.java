2974b7f4d5ddc82c1227fcb5ba850c2d335885f4
Rupam Hari <hrupam55@gmail.com>
2021-08-24 17:17:17 +0530

    added another tree (complete binary tree)

--- Tree/BinaryTree.java
+++ Tree/BinaryTree.java
@@ -1,21 +1,18 @@
 package Tree;
 
 public class BinaryTree {
+    public static Node getCompleteTree() {
+        return new Node(1, new Node(2, new Node(4), new Node(5)), new Node(3, new Node(6), new Node(7)));
+    }
+
     public static Node getTree() {
-        Node root = new Node(1);
-        Node node5 = new Node(5);
-        node5.left = new Node(6);
-        Node node2 = new Node(2);
-        node2.left = new Node(4);
-        node2.right = node5;
-        root.left = node2;
-        Node node8 = new Node(8);
-        node8.left = new Node(9);
-        node8.right = new Node(10);
-        Node node3 = new Node(3);
-        node3.left = new Node(7);
-        node3.right = node8;
-        root.right = node3;
+        Node node5 = new Node(5, new Node(6), null);
+        Node node2 = new Node(2, new Node(4), node5);
+        Node node8 = new Node(8, new Node(9), new Node(10));
+
+        Node node3 = new Node(3, new Node(7), node8);
+
+        Node root = new Node(1, node2, node3);
 
         return root;
     }
