35d2ea1e1cb1101f9aafd660cb793990c7000882
Rupam Hari <hrupam55@gmail.com>
2021-09-14 22:37:06 +0530

    change in isBalanced method

--- Tree/Height.java
+++ Tree/Height.java
@@ -13,7 +13,9 @@ public class Height {
             int check(Node node) {
                 if (node == null) return 0;
                 int lh = check(node.left);
+                if (lh == -1) return -1;
                 int rh = check(node.right);
+                if (rh == -1) return -1;
                 if (Math.abs(lh - rh) > 1) return -1;
                 return 1 + Math.max(lh, rh);
             }
