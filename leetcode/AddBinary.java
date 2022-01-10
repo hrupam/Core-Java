b4a35ae0324eed4a3e9ab3e1275f31fb56ec250a
Rupam Hari <hrupam55@gmail.com>
2022-01-11 02:53:36 +0530

    Add Binary (updated) - leetcode

--- leetcode/AddBinary.java
+++ leetcode/AddBinary.java
@@ -1,29 +1,67 @@
 package leetcode;
 
 public class AddBinary {
-    public static String addBinary(String a, String b) {
-        int num = Integer.parseInt(a, 2) + Integer.parseInt(b, 2);
-        return Integer.toBinaryString(num);
-    }
-
-    public static String addBinary2(String b1, String b2) {
-        int len1 = b1.length();
-        int len2 = b2.length();
-        int carry = 0;
-        String res = "";
-        int maxLen = Math.max(len1, len2);
-        for (int i = 0; i < maxLen; i++) {
-            int p = i < len1 ? b1.charAt(len1 - 1 - i) - '0' : 0;
-            int q = i < len2 ? b2.charAt(len2 - 1 - i) - '0' : 0;
-            int tmp = p + q + carry;
-            carry = tmp / 2;
-            res = tmp % 2 + res;
-        }
-        return (carry == 0) ? res : "1" + res;
-    }
-
 
     public static void main(String[] args) {
-        System.out.println(addBinary2("11", "1"));
+        String a = "1000000100101010101001";
+        String b = "0100110001010010";
+        System.out.println(new AddBinary().addBinary(a, b));
+    }
+
+    private static int[] getSumCarry(int a, int b, int c) {
+        int num = a + b + c;
+        if (num == 0)
+            return new int[]{0, 0};
+        if (num == 1)
+            return new int[]{1, 0};
+        if (num == 2)
+            return new int[]{0, 1};
+
+        return new int[]{1, 1};
+    }
+
+
+    public String addBinary(String a, String b) {
+
+        if (a.length() < b.length()) {
+            String t = a;
+            a = b;
+            b = t;
+        }
+
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        String result = "";
+        int carry = 0;
+        while (i >= 0 && j >= 0) {
+            int b1 = Integer.parseInt(a.charAt(i) + "");
+            int b2 = Integer.parseInt(b.charAt(j) + "");
+            int[] sumCarry = getSumCarry(b1, b2, carry);
+            int sum = sumCarry[0];
+            int nextCarry = sumCarry[1];
+
+            result = sum + result;
+            carry = nextCarry;
+
+            i--;
+
+            j--;
+
+        }
+
+        while (i >= 0) {
+            int b1 = Integer.parseInt(a.charAt(i) + "");
+            int[] sumCarry = getSumCarry(carry, b1, 0);
+            int sum = sumCarry[0];
+            int nextCarry = sumCarry[1];
+
+            result = sum + result;
+            carry = nextCarry;
+            i--;
+        }
+        if (carry == 1)
+            result = carry + result;
+
+        return result;
     }
 }
