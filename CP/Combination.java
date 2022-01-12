de8822d2b49d17cbad2c14d416cb1b2c5a2844d8
Rupam Hari <hrupam55@gmail.com>
2022-01-12 17:04:30 +0530

    added count Subsequence Sum K modified combination method

--- CP/Combination.java
+++ CP/Combination.java
@@ -7,13 +7,16 @@ import java.util.List;
 public class Combination {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3};
-        int r = 3;
+        int r = 2;
         List<List<Integer>> list = new LinkedList<>();
         combination(arr, 0, r, new ArrayList<>(), list);
         System.out.println(list);
+
+        System.out.println(countSubsequenceSumK(arr, 0, 3, 0));
     }
 
     public static void combination(int[] arr, int index, int r, List<Integer> sublist, List<List<Integer>> list) {
+        if (sublist.size() > r) return;
         if (index == arr.length) {
             if (sublist.size() == r)
                 list.add(new ArrayList<>(sublist));
@@ -25,5 +28,16 @@ public class Combination {
         sublist.remove(sublist.size() - 1);
         combination(arr, index + 1, r, sublist, list);
 
+    }
+
+    public static int countSubsequenceSumK(int[] arr, int index, int k, int sum) {
+        if (index == arr.length) {
+            if (sum == k) return 1;
+            return 0;
+        }
+        int l = countSubsequenceSumK(arr, index + 1, k, sum + arr[index]);
+        int r = countSubsequenceSumK(arr, index + 1, k, sum);
+
+        return l + r;
     }
 }
