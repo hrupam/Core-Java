d0bb9817a42f0a46e34efbf884d706c81da1afeb
Rupam Hari <hrupam55@gmail.com>
2022-02-26 01:22:54 +0530

    added isPalindrome checker helper method

--- CP/helper/Helper.java
+++ CP/helper/Helper.java
@@ -6,7 +6,6 @@
 package CP.helper;
 
 /**
- *
  * @author rupam
  */
 public class Helper {
@@ -18,5 +17,13 @@ public class Helper {
         str[j] = temp;
 
         return String.valueOf(str);
+    }
+
+    public static boolean isPalindrome(String s, int l, int r) {
+        if (l >= r) return true;
+        if (s.charAt(l) != s.charAt(r)) return false;
+
+        return isPalindrome(s, l + 1, r - 1);
+
     }
 }
