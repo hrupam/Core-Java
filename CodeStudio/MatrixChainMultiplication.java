988dbd7cca5fdedf4d2d55c2e42a5b23ab64ca2a
Rupam Hari <hrupam55@gmail.com>
2022-04-20 21:53:03 +0530

    Matrix Chain Multiplication (MCM) - Tabulation

--- CodeStudio/MatrixChainMultiplication.java
+++ CodeStudio/MatrixChainMultiplication.java
@@ -28,4 +28,20 @@ public class MatrixChainMultiplication {
         }
         return dp[i][j] = mini;
     }
+
+    public static int matrixMultiplication_Tabulation(int[] arr, int N) {
+        int[][] dp = new int[N][N];
+        for (int i = N - 1; i >= 1; i--) {
+            for (int j = i + 1; j < N; j++) {
+
+                int mini = (int) 1e9;
+                for (int k = i; k < j; k++) {
+                    int steps = (arr[i - 1] * arr[k] * arr[j]) + dp[i][k] + dp[k + 1][j];
+                    mini = Math.min(steps, mini);
+                }
+                dp[i][j] = mini;
+            }
+        }
+        return dp[1][N - 1];
+    }
 }
